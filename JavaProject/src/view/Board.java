93791aa65d8ddb8cfcd5c23f4f0123aabc12e076
Lital310 <litalshy@gmail.com>
2014-04-23 20:39:15 +0300

    update the presenter for the gui

--- JavaProject/src/view/Board.java
+++ JavaProject/src/view/Board.java
@@ -21,4 +21,12 @@ public class Board extends Canvas{
 		});
 		//...
 	} 
+	//get board data
+	public int[][] getBoardData() {
+		return boardData;
+	}
+	//set board data
+	public void setBoardData(int[][] boardData) {
+		this.boardData = boardData;
+	}
 }
